ad1edd2c6cf338c3afb5b882b124bffb222e261f
Meryem Kömür <meryemkomur@Meryems-MacBook-Pro.local>
2020-02-26 11:10:10 -0600

    README

--- js/bestseller.js
+++ js/bestseller.js
@@ -1,68 +1,39 @@
-
-// Create a lightbox
+//// Animations
 (function() {
-    var $lightbox = $("<div class='lightbox'></div>");
-    var $img = $("<img>");
-    var $caption = $("<p class='caption'></p>");
-    var $header = $("<h2 class ='header'>Testing</h2>");
-
+    'use strict';
+    var items = document.querySelectorAll('.timeline li');
   
-    // Add image and caption to lightbox
+    function isElementInViewport(el) {
+      var rect = el.getBoundingClientRect();
+      return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <=
+          (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+      );
+    }
   
-    $lightbox
-    //   .append($title) // h2
-      .append($img)
-      .append($caption)
-      .append($header)
-    
+    function callbackFunc() {
+      for (var i = 0; i < items.length; i++) {
+        if (isElementInViewport(items[i])) {
+          items[i].classList.add('in-view');
+        }
+      }
+    }
   
-    // Add lighbox to document
+    window.addEventListener('load', callbackFunc);
+    window.addEventListener('resize', callbackFunc);
+    window.addEventListener('scroll', callbackFunc);
+  })();
   
-    $('body').append($lightbox);
+  //////////////////
+  let initialBestSellers = [];
+  let valuableBestSellers = [];
+  let covers = [];
   
-    $('.lightbox-gallery img').click(function(e) {
-      e.preventDefault();
-  
-      // Get image link and description
-      var src = $(this).attr("data-image-hd");
-      var cap = $(this).attr("alt");
-      // ep's
-      let h1 = $('<h1/>').text = img.data-book-author
-  
-      // Add data to lighbox
-  
-      $img.attr('src', src);
-      $caption.text("This is info about a book \nDown here is the title\nher's author\nhere's isbn");
-      $title.text(title);
-  
-      // Show lightbox
-  
-      $lightbox.fadeIn('fast');
-  
-      $lightbox.click(function() {
-        $lightbox.fadeOut('fast');
-      });
-    });
-  
-  }());
-
-
-
-
-
-
-
-////////variables//////////
-let initialBestSellers = [];
-let valuableBestSellers = [];
-let covers = [];
-
-
-
-
-
-//functions
-function callNYTimes() {
+  //functions
+  function callNYTimes() {
     fetch(
       'https://api.nytimes.com/svc/books/v3/lists.json?list-name=hardcover-fiction&api-key=' +
         nyTimesKey,
@@ -78,21 +49,17 @@ function callNYTimes() {
         drillDownNYTimes();
       })
       .then(() => {
-        for (let index = 0; index < 12; index++) {
-            let anchorbox = [{
-                Title:valuableBestSellers[index].Title,
-                Image:"http://via.placeholder.com/100x200png"
-            }];
-            // createBookCards(anchorbox);
-
-        //   anchor.innerHTML = `
-        //   <img id="image${index}" src=${image} data-book-author="${book.author}"/><br> 
-        //   ${valuableBestSellers[index].Rank}<br>
-        //   ${valuableBestSellers[index].Title}<br>
-        //   ${initialBestSellers[0].results[index].book_details[0].author}<br>
-        //   Weeks as a Best Seller: ${initialBestSellers[0].results[index].weeks_on_list}<br>
-        //   <p class="hidden" id="isbn${index}">${valuableBestSellers[index].ISBN10}</p>
-        //   ` // Added data-book-author;
+        for (let index = 0; index < 15; index++) {
+          let card = document.getElementById(`${index}`);
+          let image = 'http://placecorgi.com/250';
+          card.innerHTML = `
+          <img id="image${index}" src=${image} /><br>
+          ${valuableBestSellers[index].Rank}.<br>
+          ${valuableBestSellers[index].Title}<br>
+          ${initialBestSellers[0].results[index].book_details[0].author}<br>
+          Weeks as a Best Seller: ${initialBestSellers[0].results[index].weeks_on_list}<br>
+          <p class="hidden" id="isbn${index}">${valuableBestSellers[index].ISBN10}</p>
+          `;
         }
       })
       .then(() => {
@@ -101,7 +68,7 @@ function callNYTimes() {
   }
   
   function drillDownNYTimes() {
-    for (let index = 0; index < 12; index++) {
+    for (let index = 0; index < 15; index++) {
       let tryObject = {};
       try {
         tryObject.Rank = initialBestSellers[0].results[index].rank;
@@ -137,14 +104,160 @@ function callNYTimes() {
   
   function pullCoversandReplaceFromOpenLibrary() {
     let openLibraryUrl = 'http://covers.openlibrary.org/b/isbn/';
-    for (let index = 0; index < 12; index++) {
-    //   let isbn = document.querySelector(`#bg${index+1}`).innerHTML;
-        let isbn = valuableBestSellers[index].ISBN10
-
-      let image = document.querySelector(`#image${index+1}`);
+    for (let index = 0; index < 15; index++) {
+      let isbn = document.querySelector(`#isbn${index}`).innerHTML;
+      let image = document.querySelector(`#image${index}`);
       image.src = openLibraryUrl + `${isbn}-M.jpg`;
     }
-
   }
   
   callNYTimes();
+// // Create a lightbox
+// (function() {
+//     var $lightbox = $("<div class='lightbox'></div>");
+//     var $img = $("<img>");
+//     var $caption = $("<p class='caption'></p>");
+//     var $header = $("<h2 class ='header'>Testing</h2>");
+
+  
+//     // Add image and caption to lightbox
+  
+//     $lightbox
+//     //   .append($title) // h2
+//       .append($img)
+//       .append($caption)
+//       .append($header)
+    
+  
+//     // Add lighbox to document
+  
+//     $('body').append($lightbox);
+  
+//     $('.lightbox-gallery img').click(function(e) {
+//       e.preventDefault();
+  
+//       // Get image link and description
+//       var src = $(this).attr("data-image-hd");
+//       var cap = $(this).attr("alt");
+//       // ep's
+//       let h1 = $('<h1/>').text = img.data-book-author
+  
+//       // Add data to lighbox
+  
+//       $img.attr('src', src);
+//       $caption.text("This is info about a book \nDown here is the title\nher's author\nhere's isbn");
+//       $title.text(title);
+  
+//       // Show lightbox
+  
+//       $lightbox.fadeIn('fast');
+  
+//       $lightbox.click(function() {
+//         $lightbox.fadeOut('fast');
+//       });
+//     });
+  
+//   }());
+
+
+
+
+
+
+
+// ////////variables//////////
+// let initialBestSellers = [];
+// let valuableBestSellers = [];
+// let covers = [];
+
+
+
+
+
+// //functions
+// function callNYTimes() {
+//     fetch(
+//       'https://api.nytimes.com/svc/books/v3/lists.json?list-name=hardcover-fiction&api-key=' +
+//         nyTimesKey,
+//       { method: 'get' }
+//     )
+//       .then(response => {
+//         return response.json();
+//       })
+//       .then(json => {
+//         initialBestSellers.push(json);
+//       })
+//       .then(() => {
+//         drillDownNYTimes();
+//       })
+//       .then(() => {
+//         for (let index = 0; index < 12; index++) {
+//             let anchorbox = [{
+//                 Title:valuableBestSellers[index].Title,
+//                 Image:"http://via.placeholder.com/100x200png"
+//             }];
+//             // createBookCards(anchorbox);
+
+//         //   anchor.innerHTML = `
+//         //   <img id="image${index}" src=${image} data-book-author="${book.author}"/><br> 
+//         //   ${valuableBestSellers[index].Rank}<br>
+//         //   ${valuableBestSellers[index].Title}<br>
+//         //   ${initialBestSellers[0].results[index].book_details[0].author}<br>
+//         //   Weeks as a Best Seller: ${initialBestSellers[0].results[index].weeks_on_list}<br>
+//         //   <p class="hidden" id="isbn${index}">${valuableBestSellers[index].ISBN10}</p>
+//         //   ` // Added data-book-author;
+//         }
+//       })
+//       .then(() => {
+//         pullCoversandReplaceFromOpenLibrary();
+//       });
+//   }
+  
+//   function drillDownNYTimes() {
+//     for (let index = 0; index < 12; index++) {
+//       let tryObject = {};
+//       try {
+//         tryObject.Rank = initialBestSellers[0].results[index].rank;
+//       } catch {
+//         tryObject.Rank = 'Rank Unavaliable.';
+//       }
+//       try {
+//         tryObject.WeeksOnList =
+//           initialBestSellers[0].results[index].weeks_on_list;
+//       } catch {
+//         tryObject.WeeksOnList = 'Ammount of time on the list is unknown.';
+//       }
+//       try {
+//         tryObject.ISBN10 = initialBestSellers[0].results[index].isbns[0].isbn10;
+//       } catch {
+//         tryObject.ISBN10 = 'ISBN information unavaliable.';
+//       }
+//       try {
+//         tryObject.Title =
+//           initialBestSellers[0].results[index].book_details[0].title;
+//       } catch {
+//         tryObject.Title = 'Title unavaliable.';
+//       }
+//       try {
+//         tryObject.Author =
+//           initialBestSellers[0].results[index].book_details[0].author;
+//       } catch {
+//         tryObject.Author = 'Author information unavaliable.';
+//       }
+//       valuableBestSellers.push(tryObject);
+//     }
+//   }
+  
+//   function pullCoversandReplaceFromOpenLibrary() {
+//     let openLibraryUrl = 'http://covers.openlibrary.org/b/isbn/';
+//     for (let index = 0; index < 12; index++) {
+//     //   let isbn = document.querySelector(`#bg${index+1}`).innerHTML;
+//         let isbn = valuableBestSellers[index].ISBN10
+
+//       let image = document.querySelector(`#image${index+1}`);
+//       image.src = openLibraryUrl + `${isbn}-M.jpg`;
+//     }
+
+//   }
+  
+//   callNYTimes();
--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 let searchButton = document.querySelector('#button1');
 let object = [];
 let valObject = [];
+let valObject2 = [];
 //////////Google
 //Functions
 function drillDownGoogle() {
@@ -13,9 +14,24 @@ function drillDownGoogle() {
       tryObject.Title = 'Title Unavaliable.';
     }
     try {
+      tryObject.Subtitle = objects[0].items[index].volumeInfo.subtitle;
+    } catch {
+      tryObject.Subtitle = 'Subtitle N/A.';
+    }
+    try {
+      tryObject.Description = objects[0].items[index].volumeInfo.description;
+    } catch {
+      tryObject.Description = 'Description unavaliable.';
+    }
+    try {
       tryObject.Author = object[0].items[index].volumeInfo.authors[0];
     } catch {
       tryObject.Author = 'Author unknown.';
+    }
+    try {
+      tryObject.Pages = objects[0].items[index].volumeInfo.pageCount;
+    } catch {
+      tryObject.Pages = 'Page Count Unavaliable';
     }
     try {
       tryObject.Published = object[0].items[index].volumeInfo.publishedDate;
@@ -34,6 +50,7 @@ function drillDownGoogle() {
       tryObject.Image = 'images/placeholder.jpg';
     }
     valObject.push(tryObject);
+    valObject2.push(tryObject);
     console.log(valObject);
   }
 }
@@ -46,11 +63,32 @@ function createBookCards(objToCreateFrom) {
               <div class="card">
                 <img class="card--avatar" src=${Image} />
                 <h1 class="card--title">${Title}</h1>
-                <a class="card--link" href="#">Timesify</a>
+                <a class="card--link" href="#open-modal">View Info</a>
               </div>
               `)
   );
   container.innerHTML = output;
+}
+function populateModal(populatingItem) {
+  modalContent = `
+  <div id='open-modal' class='modal-window'>
+    <div>
+      <a href='#modal-close' title='Close' class='modal-close'>
+        close &times;
+      </a>
+      <h1>${populatingItem.Title || 'Title not avaliable.'}</h1>
+      <div>${populatingItem.Subtitle || 'Subtitle not avaliable.'}</div><br>
+      <div>Description: ${populatingItem.Description ||
+        'Description not avaliable.'}</div><br>
+      <div>By: ${populatingItem.Author || 'Author not avaliable.'}</div><br>
+      <div>Page count: ${populatingItem.Pages ||
+        'Page count unknown.'}</div><br> 
+      <div>Published on: ${populatingItem.Published ||
+        'Publishing date unknown.'}</div><br>
+      <div>ISBN: ${populatingItem.ISBN_10 || 'ISBN unknown.'}
+    </div>
+  </div>;`;
+  cardsContainer.insertAdjacentHTML('afterend', modalContent);
 }
 
 
@@ -74,7 +112,35 @@ searchButton.addEventListener('click', e => {
       // object with title and Title and image
       createBookCards(valObject);
     })
+    .then(() => {
+      objects = [];
+      valObject = [];
+    })
     .catch(() => {
       console.log('error');
     });
+});
+
+cardsContainer.addEventListener('click', e => {
+  let cardButton = document.querySelectorAll('.card--link');
+  console.log(valObject2);
+  if (e.target.className == 'card--link' && e.target == cardButton[0]) {
+    populateModal(valObject2[0]);
+  } else if (e.target.className == 'card--link' && e.target == cardButton[1]) {
+    populateModal(valObject2[1]);
+  } else if (e.target.className == 'card--link' && e.target == cardButton[2]) {
+    populateModal(valObject2[2]);
+  } else if (e.target.className == 'card--link' && e.target == cardButton[3]) {
+    populateModal(valObject2[3]);
+  } else if (e.target.className == 'card--link' && e.target == cardButton[4]) {
+    populateModal(valObject2[4]);
+  } else if (e.target.className == 'card--link' && e.target == cardButton[5]) {
+    populateModal(valObject2[5]);
+  } else if (e.target.className == 'card--link' && e.target == cardButton[6]) {
+    populateModal(valObject2[6]);
+  } else if (e.target.className == 'card--link' && e.target == cardButton[7]) {
+    populateModal(valObject2[7]);
+  } else if (e.target.className == 'card--link' && e.target == cardButton[8]) {
+    populateModal(valObject2[8]);
+  }
 });
